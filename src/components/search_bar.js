7e55f569660b63b8ba912aff0f91c64d1929f44d
jasonlee <jlee7060@gmail.com>
2021-05-18 18:52:56 -0400

    autoindent package added

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -1,39 +1,42 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 //const Component = React.Component;
 // input type bar on top of the page.
 //const SearchBar = () => {
 //    return <input />
 //};
-
 //class SearchBar extends React.Component {
-class SearchBar extends Component{
-    constructor(props){
+class SearchBar extends Component {
+  constructor(props) {
     super(props);
 
-    this.state = { term: ''};
-    }
-// render function acts like function method. This also will return JSX
-    render() {
+    this.state = {
+      term: "123",
+    };
+  }
+  // render function acts like function method. This also will return JSX
+  render() {
     // conecpt of props. property = onChange(react defined proected property)
-//      return <input onChange={this.onInputChange} />;
-//        return <input onChange={event => console.log(event.target.value)}/>;
-        return(
-        <div>
-        <input onChange={event=>this.setState({term:event.target.value})} />
-//        use setState instead of set the variable directly
-        InputValue : {this.state.term}
-        </div>
-
-        );
-
-    }
-//    e or event
-// handleInputChange
-//    onInputChange(event){
-//        console.log(event.target.value);
-////    console.log(event);
-//    }
+    //      return <input onChange={this.onInputChange} />;
+    //        return <input onChange={event => console.log(event.target.value)}/>;
+    return (
+      <div>
+        <input
+          value={this.state.term}
+          onChange={(event) =>
+            this.setState({
+              term: event.target.value,
+            })
+          }
+        />{" "}
+      </div>
+    );
+  }
+  //    e or eventıå
+  // handleInputChange
+  //    onInputChange(event){
+  //        console.log(event.target.value);
+  ////    console.log(event);
+  //    }
 }
 //new SearchBar
-
 export default SearchBar;
--- src/index.js
+++ src/index.js
@@ -1,9 +1,8 @@
 //ES6
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
 
-
-import SearchBar from './components/search_bar';
+import SearchBar from "./components/search_bar";
 //Create a new component. This component should prduce some HTML
 //const is ES6 syntex. const = final value
 //JSX : subset of dialect from JS which looks like HTML
@@ -19,16 +18,15 @@ import SearchBar from './components/search_bar';
 //const vs let vs var
 //https://dev.to/anuradhasivasubramanian/5-things-to-remember-when-using-an-env-file-to-store-you-api-key-in-a-react-app-4f2o
 const API_KEY = process.env.API_KEY;
-const App = ()=>{
-return (<div>
-            <SearchBar />
-        </div>);
-}
+const App = () => {
+  return (
+    <div>
+      <SearchBar />
+    </div>
+  );
+};
 //Take this compoenent's generated HTML and put it on the page(in the DOM)
 
 //ReactDOM.render(App);
 //App is class. to make it instance, use <App />. wrap up with JSX tag
-ReactDOM.render(<App />, document.querySelector('.container'));
-
-
-
+ReactDOM.render(<App />, document.querySelector(".container"));
